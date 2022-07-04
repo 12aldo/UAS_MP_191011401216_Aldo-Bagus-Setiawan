0cbb52e58a10d100260b467d4e81fc6aa150e371
12aldo <103941369+12aldo@users.noreply.github.com>
2022-07-04 23:39:25 +0700

    Add files via upload

---
+++ lib/providers/berita_provider.dart
@@ -0,0 +1,21 @@
+import 'package:uasmp/models/berita_model.dart';
+import 'package:uasmp/services/berita_services.dart';
+import 'package:flutter/cupertino.dart';
+
+class ArtikelProvider extends ChangeNotifier {
+  ArtikelProvider() {
+    showListArtikel();
+  }
+
+  final ArtikelService _artikelService = ArtikelService();
+  artikel Artikel = artikel();
+
+  showListArtikel() async {
+    Artikel = await _artikelService.tampilkanartikel();
+  }
+
+  ShowDetailArtikel({String? inputKey}) async {
+    Artikel = await _artikelService.tampilkandetail(key: inputKey);
+    notifyListeners();
+  }
+}
